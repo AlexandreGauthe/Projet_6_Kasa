822eb5f854a4737abeaadf01453d5836c816e1e9
AlexandreGauthe <alexandre.gauthe@outlook.fr>
2024-04-12 16:57:40 +0200

    corrections images home + collapse et json

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,14 @@
 import React from "react";
 import logements from "../data/logements.json";
-import "../styles/Card.scss"
+import "../styles/Card.scss";
+import { Link } from "react-router-dom";
 
 export default function Card(props) {
     return(
         logements.map((logement) =>(
             <div className="cards" key={logement.id}>
-               <p className="cards__title">{logement.title}</p>    
+               <Link  className="cards__carroussel-link" to="/accomodations"><img src={logement.cover} alt={logement.title}></img> <p className="cards__cards-title">{logement.title}</p></Link>
+               
             </div>    
         ))
     )
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,7 +3,7 @@ import Header from "../../components/Header";
 import Banner from "../../components/Banner";
 import Card from "../../components/Card";
 import Footer from "../../components/Footer";
-import "../../styles/Home.scss"
+import "../../styles/Home.scss";
 
 export default  function Home() {
   return (
